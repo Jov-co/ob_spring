02d404aa9cb29eba39bda9ff569ed5ad98cced6a
Johan Vasquez <johanjvc7@gmail.com>
2022-10-12 15:56:03 -0500

    Initial commit

---
+++ src/main/java/com/example/ob_spring/ObSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ob_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ob_spring/ObSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ob_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
